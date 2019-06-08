4bbefaa4c20d49a12ca7c9ca8237f326eb3634e3
Timothy Levins <timothy.levins@invenia.ca>
2019-06-08 15:55:08 -0500

    added events stub

--- app/src/main/java/com/example/memedatabase/DBLoader.java
+++ app/src/main/java/com/example/memedatabase/DBLoader.java
@@ -5,14 +5,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 
 public class DBLoader {
     // loads the "cards.json" file into a java string.
-    private static String loadJSONFromAsset(Context context) {
+    private static String loadJSONFromAsset(Context context, String filename) {
         String json = null;
         try {
-            InputStream is = context.getAssets().open("cards.json");
+            InputStream is = context.getAssets().open(filename);
             int size = is.available();
             byte[] buffer = new byte[size];
             is.read(buffer);
@@ -55,7 +56,38 @@ public class DBLoader {
 
     // convenience method
     public static void loadMasterDeck(MasterDeckInterface db, Context context) {
-        String json = DBLoader.loadJSONFromAsset(context);
+        String json = DBLoader.loadJSONFromAsset(context, "cards.json");
         DBLoader.loadMasterDeck(db, json);
+    }
+
+    // Takes a json encoded string of card objects and adds it to the eventslist db.
+    // Ony inserts an event if it doesn't exist in the db.
+    private static void loadEventsList(EventListInterface db, String jsonString){
+        try {
+            // load json encoded string
+            JSONObject json_obj = new JSONObject(jsonString);
+            JSONArray json_events = json_obj.getJSONArray("events");
+            String name, desc, tag;
+            boolean positive;
+            JSONObject json_event;
+            ArrayList<String> events = db.retrieveAllEventNames();
+
+            for (int i = 0; i < json_events.length(); i++) {
+                // extract json data
+                json_event = json_events.getJSONObject(i);
+                name = json_event.getString("name");
+                // only continue if events doesn't evist
+                if (!events.contains(name)) {
+                    desc = json_event.getString("description");
+                    positive = json_event.getBoolean("positive");
+                    tag = json_event.getString("tag");
+
+                    // insert into db
+                    db.insertEvent(name, desc, tag, positive);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
