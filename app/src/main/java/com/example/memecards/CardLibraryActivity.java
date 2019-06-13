a4dc0831a791d8057ba2166f92eca26d138518c3
Timothy Levins <timothy.levins@invenia.ca>
2019-06-13 12:38:37 -0500

    added free unlock feature

--- app/src/main/java/com/example/memecards/CardLibraryActivity.java
+++ app/src/main/java/com/example/memecards/CardLibraryActivity.java
@@ -8,14 +8,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 import com.example.domainobjects.MemeCard;
 import com.example.memedatabase.DBLoader;
+import com.example.memedatabase.MasterDeckInterface;
 import com.example.memedatabase.MasterDeckStub;
 
 public class CardLibraryActivity extends AppCompatActivity {
     private ArrayList<MemeCard> cards = new ArrayList<>();
+    private static MasterDeckInterface masterDeck = new MasterDeckStub();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +27,14 @@ public class CardLibraryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_card_library);
 
         // load DB into static stub
-        MasterDeckStub masterDeck = new MasterDeckStub();
-        DBLoader.loadMasterDeck(masterDeck, this.getApplicationContext());
+        DBLoader.loadMasterDeck(this.masterDeck, this.getApplicationContext());
         MemeCard card;
-        for (String n : masterDeck.retrieveAllCardNames()) {
-            System.out.println(n);
-            card = masterDeck.retrieveCard(n);
+        for (String n : this.masterDeck.retrieveAllCardNames()) {
+            card = this.masterDeck.retrieveCard(n);
             this.cards.add(card);
         }
 
-        showCardStats(masterDeck);
+        showCardStats();
         MakeCardsList();
     }
 
@@ -44,12 +46,15 @@ public class CardLibraryActivity extends AppCompatActivity {
     }
 
     // Show the number of total cards and locked cards
-    private void showCardStats(MasterDeckStub masterDeck) {
+    private void showCardStats() {
         TextView unlockedText = findViewById(R.id.UnlockedText);
         TextView unlockedNum = findViewById(R.id.UnlockedNum);
 
         unlockedText.setText("Unlocked Cards");
-        unlockedNum.setText(":  " + masterDeck.retrieveUnlockedCardNames().size() + " / " + masterDeck.deckSize());
+        unlockedNum.setText(
+                ":  " + this.masterDeck.retrieveUnlockedCardNames().size() +
+                        " / " + this.masterDeck.deckSize()
+        );
     }
 
     private void MakeCardsList() {
@@ -65,6 +70,32 @@ public class CardLibraryActivity extends AppCompatActivity {
     protected void onResume () {
         super.onResume();
         hideActionBar();
+    }
+
+    public void unlockCard(View v) {
+        String new_card = null;
+        for (MemeCard card : this.cards){
+            if (card.isLocked()) {
+                masterDeck.unlockCard(card.getName());
+                new_card = card.getName();
+                Toast.makeText(
+                        CardLibraryActivity.this,
+                        "Congratulations, you have unlocked " + new_card + "!!",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        if (new_card == null)
+            Toast.makeText(
+                    CardLibraryActivity.this,
+                    "You've already unlocked all cards!!!",
+                    Toast.LENGTH_SHORT).show();
+        else {
+            //update recycler view
+            MakeCardsList();
+            showCardStats();
+        }
+
     }
 
     /** Hides the status bar and action bar for an activity**/
